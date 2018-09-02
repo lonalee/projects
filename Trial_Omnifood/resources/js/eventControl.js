0469f042c0d84ee417f7ab4231ac7a35797bfb34
lonalee2018 <lonalee2018@gmail.com>
2018-09-02 16:33:53 +0900

    slideRight DONE

--- Trial_Omnifood/resources/js/eventControl.js
+++ Trial_Omnifood/resources/js/eventControl.js
@@ -9,8 +9,8 @@ menuClick.addEventListener('mouseover', function() {
   // <li class="sub-menu"><a href="">Our cities</a></li>
   // <li class="sub-menu"><a href="">Sign up</a></li>
   // </div>`
-  // console.dir(this.childNodes[3].className);
-  this.childNodes[3].className = 'show-list'
+  // console.dir(this.childNodes);
+  this.childNodes[3].childNodes[1].className = 'show-list'
 });
 
 // const menuOut = document.querySelector('div.sub-menu-list');
@@ -22,7 +22,30 @@ menuClick.addEventListener('mouseover', function() {
 var showList = document.querySelector('li.sub-menu');
 menuClick.addEventListener('mouseout', function() {
   // this.parentNode.className = 'sub-menu-list';
-  this.childNodes[3].className = 'sub-menu-list'
-  console.dir(this.childNodes[3].className);
+  this.childNodes[3].childNodes[1].className = 'sub-menu-list'
+  // console.dir(this.childNodes[3].className);
+})
 
+const slideRight = document.querySelector('button.btn-right')
+const slideLeft = document.querySelector('button.btn-left')
+// bubbling으로 이벤트를 캐치할 수도 있다. 그러면 DOM 선택을 하나만 해도 될 것이다. 수정필요.
+let n = 886;
+
+slideRight.addEventListener('click', (e) => {
+
+  if (e.target.parentNode.childNodes[7].childNodes[1].style.transform !== '') {
+    n = n + 886;
+    e.target.parentNode.childNodes[7].childNodes[1].style.transform = `translateX(-${n}px)`;
+    e.target.parentNode.childNodes[7].childNodes[1].style.transition = `transform 1s`
+    console.log('not first', n);
+    return n
+  }
+  // 누적되는 이동 px을 전역변수에 재할당하기 위해서 return 한다.. 전역변수를 사용 안 하려면? 계속 이동하기 위해서는 px 값을 계속 저장하고 있어야 함
+  //
+
+  if (e.target.parentNode.childNodes[7].childNodes[1].style.transform === '') {
+    e.target.parentNode.childNodes[7].childNodes[1].style.transform = `translateX(-${n}px)`;
+    e.target.parentNode.childNodes[7].childNodes[1].style.transition = `transform 1s`
+    console.log('first', n)
+  }
 })
